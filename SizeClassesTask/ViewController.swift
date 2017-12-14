96a3461741b1d53919145f9a3fa14fd287f74e20
Robert Berry <robertcberry@yahoo.com>
2017-12-14 15:43:41 -0600

    Initial Commit

---
+++ SizeClassesTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassesTask
+//
+//  Created by Robert Berry on 12/6/17.
+//  Copyright © 2017 Robert Berry. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
